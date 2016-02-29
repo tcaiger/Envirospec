4872342af80ead9c339527f1632a788232dc8720
Tom <caigertom@gmail.com>
2016-02-29 16:47:08 +1300

    test

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,28 +1,61 @@
 <?php
 class Page extends SiteTree {
 
+	
 
 }
 
 
 class Page_Controller extends ContentController {
 
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+	private static $allowed_actions = array (
+	);
 
 	public function init() {
 		parent::init();
 
+		//////////////////////////////////////////////////////////// 
 		// CSS Includes 
+		//////////////////////////////////////////////////////////// 
+
+		// Custom CSS
+		Requirements::css("{$this->ThemeDir()}/css/main.min.css");
+
 		Requirements::combine_files(
-		    'main.css',
+		    'main.js',
 		    array(
-		        "{$this->ThemeDir()}/css/main.min.css",
 		        "{$this->ThemeDir()}/font-awesome/css/font-awesome.min.css",
 		        "{$this->ThemeDir()}/css/animate.css",
 		        "{$this->ThemeDir()}/css/magnific-popup.css"
+
 		    )
 		);
- 
+		// // Font Awesome For Icons
+		// Requirements::css("{$this->ThemeDir()}/font-awesome/css/font-awesome.min.css");
+		// // Animated CSS
+		// Requirements::css("{$this->ThemeDir()}/css/animate.css");
+		// // Popups Css
+		// Requirements::css("{$this->ThemeDir()}/css/magnific-popup.css");
+
+
+		//////////////////////////////////////////////////////////// 
 		// JS Includes 
+		//////////////////////////////////////////////////////////// 
 		Requirements::combine_files(
 		    'main.js',
 		    array(
@@ -49,4 +82,5 @@ class Page_Controller extends ContentController {
 					'ParentID' => 107
 				));
 	}
+
 }
