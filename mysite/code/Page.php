a96b3b24b22af0201a8b52ae69114e2baffe58ca
Tom <caigertom@gmail.com>
2016-02-29 16:38:40 +1300

    Changed Permissions

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,52 +1,28 @@
 <?php
 class Page extends SiteTree {
 
-	
 
 }
 
 
 class Page_Controller extends ContentController {
 
-	/**
-	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-	 * permissions or conditions required to allow the user to access it.
-	 *
-	 * <code>
-	 * array (
-	 *     'action', // anyone can access this action
-	 *     'action' => true, // same as above
-	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-	 * );
-	 * </code>
-	 *
-	 * @var array
-	 */
-	private static $allowed_actions = array (
-	);
 
 	public function init() {
 		parent::init();
 
-		//////////////////////////////////////////////////////////// 
 		// CSS Includes 
-		//////////////////////////////////////////////////////////// 
-
-		// Custom CSS
-		Requirements::css("{$this->ThemeDir()}/css/main.min.css");
-		// Font Awesome For Icons
-		Requirements::css("{$this->ThemeDir()}/font-awesome/css/font-awesome.min.css");
-		// Animated CSS
-		Requirements::css("{$this->ThemeDir()}/css/animate.css");
-		// Popups Css
-		Requirements::css("{$this->ThemeDir()}/css/magnific-popup.css");
-
-
-		//////////////////////////////////////////////////////////// 
+		Requirements::combine_files(
+		    'main.css',
+		    array(
+		        "{$this->ThemeDir()}/css/main.min.css",
+		        "{$this->ThemeDir()}/font-awesome/css/font-awesome.min.css",
+		        "{$this->ThemeDir()}/css/animate.css",
+		        "{$this->ThemeDir()}/css/magnific-popup.css"
+		    )
+		);
+ 
 		// JS Includes 
-		//////////////////////////////////////////////////////////// 
-
 		Requirements::combine_files(
 		    'main.js',
 		    array(
@@ -60,21 +36,6 @@ class Page_Controller extends ContentController {
 
 		    )
 		);
-
-		// // JQuery
-	 //    Requirements::javascript("{$this->ThemeDir()}/js/jquery.min.js");
-	 //    // Bootstrap
-	 //    Requirements::javascript("{$this->ThemeDir()}/bootstrap/js/bootstrap.min.js");
-	 //    // Easing Plugin For Smooth Scroll
-	 //    Requirements::javascript("{$this->ThemeDir()}/js/jquery.easing.min.js");
-	 //    // On Scroll Animation
-	 //    Requirements::javascript("{$this->ThemeDir()}/js/wow.min.js");
-	 //    // image loads plugin
-	 //    Requirements::javascript("{$this->ThemeDir()}/js/imagesloaded.min.js");
-	 //    // magnific pop up
-	 //    Requirements::javascript("{$this->ThemeDir()}/js/jquery.magnific-popup.min.js");
-	 //    // Custom JS
-	 //    Requirements::javascript("{$this->ThemeDir()}/js/custom.js");
 	}
 
 	public function GetProdCats(){
@@ -88,5 +49,4 @@ class Page_Controller extends ContentController {
 					'ParentID' => 107
 				));
 	}
-
 }
