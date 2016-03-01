a06fc7c201824836ecd4230d1bd23a9f0ede53ea
Tom <caigertom@gmail.com>
2016-03-01 13:36:44 +1300

    Updated Site Config

--- mysite/code/SiteConfigExtension.php
+++ mysite/code/SiteConfigExtension.php
@@ -1,2 +1,26 @@
+<?php
+
+
+class SiteConfigExtension extends DataExtension {
+
+
+	private static $db = array(
+		'ESSummary' => 'Text',
+		'ContactAddress' => 'Text',
+		'ContactEmail' => 'Varchar',
+		'ContactFormEmail' => 'Varchar',
+		'ContactPhone' => 'Varchar'
+	);
+
+	public function updateCMSFields(Fieldlist $fields){
+		$fields->addFieldsToTab('Root.Main', array(
+			TextAreaField::create('ESSummary', 'Envirospec Summary'),
+			TextAreaField::create('ContactAddress', 'Contact Address'),
+			TextField::create('ContactEmail', 'Contact Email'),
+			TextField::create('ContactFormEmail', 'Contact Form Email'),
+			TextField::create('ContactPhone', 'Contact Phone')
+		));
+	}
+}
 
 
