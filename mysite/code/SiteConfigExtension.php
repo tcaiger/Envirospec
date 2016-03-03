0d118a535ec6d31e740289103069bb3f7cfebcd3
Tom <caigertom@gmail.com>
2016-03-03 17:27:44 +1300

    Site Config2

--- mysite/code/SiteConfigExtension.php
+++ mysite/code/SiteConfigExtension.php
@@ -28,6 +28,31 @@ class SiteConfigExtension extends DataExtension {
 			TextField::create('ContactFormEmail', 'Contact Form Email'),
 			TextField::create('ContactPhone', 'Contact Phone')
 		));
+
+		$fields->addFieldsToTab('Root.Emails', array(
+			LabelField::create('Label1', 'Automated Reminder Emails')->addExtraClass('customBold'),
+			LabelField::create('Label2', 'Will be sent to all product suppliers to inform them that they need to review and sign off theit products.'),
+			ToggleCompositeField::create('Month', 'One Month Reminder Email', array (
+
+				DateField::create('MonthReminderDate', 'Date')->setConfig('showcalendar', true),
+				HTMLEditorField::create('MonthReminderText', 'Email Message')
+			))->setStartClosed(true),
+			ToggleCompositeField::create('Week', 'Two Week Reminder Email', array (
+
+				DateField::create('WeekReminderDate', 'Date')->setConfig('showcalendar', true),
+				HTMLEditorField::create('WeekReminderText', 'Email Message')
+			)),
+			ToggleCompositeField::create('Final', 'Final Reminder Email', array (
+
+				DateField::create('FinalReminderDate', 'Date')->setConfig('showcalendar', true),
+				HTMLEditorField::create('FinalReminderText', 'Email Message')
+			)),
+			LabelField::create('Label3', 'One Off Email')->addExtraClass('customBold'),
+			LabelField::create('Label4', 'Will be sent to all product suppliers.'),
+			ToggleCompositeField::create('Custom', 'One Off Email', array (
+				HTMLEditorField::create('CustomText', 'Email Message')
+			))->setStartClosed(true)
+		));
 		
 	}
 }
