503f336ec806b7aa5b9be721be8bb26e925b40be
Tom <caigertom@gmail.com>
2016-03-03 17:26:21 +1300

    SiteConfig1

--- mysite/code/SiteConfigExtension.php
+++ mysite/code/SiteConfigExtension.php
@@ -20,6 +20,14 @@ class SiteConfigExtension extends DataExtension {
 	);
 
 	public function updateCMSFields(Fieldlist $fields){
+
+		$fields->addFieldsToTab('Root.Main', array(
+			TextAreaField::create('ESSummary', 'Envirospec Summary'),
+			TextAreaField::create('ContactAddress', 'Contact Address'),
+			TextField::create('ContactEmail', 'Contact Email'),
+			TextField::create('ContactFormEmail', 'Contact Form Email'),
+			TextField::create('ContactPhone', 'Contact Phone')
+		));
 		
 	}
 }
